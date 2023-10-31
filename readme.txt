ebc045174f282d1e30f11f441dc7eca99e596011
LeoKingzc1 <88300284+LeoKingzc1@users.noreply.github.com>
2023-10-31 15:30:51 +0800

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,2 @@
+1.cd report_of_finance
+2.python run_demo.py
---
+++ run_demo.py
@@ -0,0 +1,11 @@
+from src.create_ui import create_ui
+
+def main():
+    demo = create_ui()
+    demo.queue()
+    demo.launch()
+
+
+if __name__ == "__main__":
+    
+    main()
